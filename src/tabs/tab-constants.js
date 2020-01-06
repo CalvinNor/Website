c016d41f0c9d1a9afe59e773d5b1989ac2de7384
Calvin Noronha <calvin.nrnha@gmail.com>
2020-01-06 19:21:41 +0530

    Updates

--- src/tabs/tab-constants.js
+++
@@ -1,9 +0,0 @@
-const TAB_ABOUT = 0;
-const TAB_CONTRIBUTIONS = 1;
-const TAB_CONTACT = 2;
-
-const TAB_NAMES = ["About", "Contributions", "Contact"];
-
-export {
-    TAB_ABOUT, TAB_CONTACT, TAB_CONTRIBUTIONS, TAB_NAMES
-}
