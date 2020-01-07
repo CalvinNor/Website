871c7277a3eb7f6f2f72a5188039672447c6d326
Calvin Noronha <calvin.nrnha@gmail.com>
2020-01-07 15:24:55 +0530

    :bug: Update Twitter Link

--- src/contact/component.js
+++ src/contact/component.js
@@ -8,7 +8,7 @@ class Contact extends Component {
     render() {
         return (
             <Card className="card">
-                <SocialIcon className="social-icon" url="https://twitter.com/calvinnrn"/>
+                <SocialIcon className="social-icon" url="https://twitter.com/calvinnrnha"/>
                 <SocialIcon className="social-icon" url="https://www.linkedin.com/in/calvin-noronha/"/>
                 <SocialIcon className="social-icon" url="https://github.com/CalvinNor"/>
                 <SocialIcon className="social-icon" url="https://medium.com/@calvin.nrnha"/>
